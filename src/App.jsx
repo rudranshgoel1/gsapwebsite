151fa6e2cc1aeefa09c4e7a79fa99b8c491076e7
rudranshgoel1 <app.whatsupleads@gmail.com>
2026-07-28 00:46:21 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
